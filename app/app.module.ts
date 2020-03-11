69625ddaead74490174bee7c91b56e8957c71021
maheralias <kejayaansebenar@gmail.com>
2020-03-12 01:57:16 +0800

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,7 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+testtttttt
 @NgModule({
   declarations: [
     MyApp,
